c300cd130fc12e9ea8109365a65ac6dcb5378905
GitHub Action <action@github.com>
2024-10-21 12:07:29 +0000

    Revert "修正函数名EvaluateString"
    This reverts commit 8497d9b938b5e47673596a52c9955aca6dd07b9a.

--- src/parenthesisMatch.cpp
+++ src/parenthesisMatch.cpp
@@ -130,7 +130,7 @@ std::string ReplaceDeepestParenthesisByString(std::string tmpstr1, std::string t
 	return tempstring;
 }
 
-std::string EvaluateString(std::string tmpstr) {
+std::string EvaulateString(std::string tmpstr) {
 	return ReplaceDeepestParenthesisByString(tmpstr, additionCut(FindDeepestParenthesis(tmpstr)));
 
 }
