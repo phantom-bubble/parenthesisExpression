62709875eb8a9a5b9bab61cabd2aba15a04c3c14
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-21 15:29:39 +0800

    Create parenthesisMatch.h

---
+++ parenthesisMatch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+/**
+ * @brief  匹配括号
+ * @param  tmpstr 待匹配的字符串
+ * @return bool   匹配成功返回true，否则返回false
+ */
+bool parenthesisMatch(std::string tmpstr);
+
+std::string FindDeepestParenthesis(std::string tmpstr, bool option=1);
+
+std::string addtionCut(std::string tmpstr);
+
+//删除空格
+std::string removeSpace(std::string tmpstr);
+
+//找到最深层括号的索引
+int findDeepestParenthesisIndex(std::string tmpstr);
+
+std::string ReplaceDeepestParenthesisByString(std::string tmpstr1, std::string tmpstr2);
+
+std::string EvaulateString(std::string tmpstr);
