db8cea1755ea0eba7e315fc6dbd345f7a7e41291
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-21 16:13:50 +0800

    Update additionCut.cpp

--- tests/additionCut.cpp
+++ tests/additionCut.cpp
@@ -12,11 +12,13 @@ int main() {
       std::string sum;
       std::string var1;
       std::string var2;
-      std::ostringstream os;
-      os << i << std::flush;
+      std::ostringstream os1(var1);
+      os1 << i << std::flush;
       var1 = os.str();
+      std::ostringstream os2(var2);
       os << j << std::flush;
       var2 = os.str();
+      std::ostringstream ossum(sum);
       os << i + j << std::flush;
       sum = os.str();
       ASSERT(sum, additionCut(var1 + "+" + var2))
