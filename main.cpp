a271e41d8628f238aa360176e8ca0131d74c0862
phantom-bubble <166215777+phantom-bubble@users.noreply.github.com>
2024-10-22 00:21:29 +0800

    故意写错

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main() {
 	int a(1);
 	std::string test = "(1$我锟斤拷-jab2(*烫烫烫dhidoaozzzZzw)-;2(&iwh）中文括号（72))()";
-//	std::cout << findDeepestParenthesisIndex(test) << std::endl;
+//	std::cout << findDeepestPar123enthesisIndex(test) << std::endl;
 //	std::cout << findDeepestParenthesisIndex("((1+2)*3)") << std::endl;
 	std::cout << EvaluateString("1+2+3+(4-5)*6") << std::endl;
 	std::cout << ReplaceDeepestParenthesisByString("1+2*(3+4)", additionCut(FindDeepestParenthesis("(1+2*(3+4))"))) << std::endl;
