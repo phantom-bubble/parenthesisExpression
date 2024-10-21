4db378c7261ece4c59cfae4841edd4aebff73526
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-21 16:26:09 +0800

    Update additionCut.cpp

--- tests/additionCut.cpp
+++ tests/additionCut.cpp
@@ -7,8 +7,8 @@
 #define ASSERT(var1, var2) if(var1 != var2) { std::cout << var1 << var2 << "\n"; exit(-1); }
 
 int main() {
-  for(int i = 0; i < 1024; ++i) {
-    for(int j = 0; j < 1024; ++j) {
+  for(int i = 0; i < 2048; ++i) {
+    for(int j = 0; j < 2048; ++j) {
       std::string sum;
       std::string var1;
       std::string var2;
