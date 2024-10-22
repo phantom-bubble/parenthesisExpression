3588647890ca6aaed21a9dd79c8c6c2aed044fb8
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-22 22:04:44 +0800

    Update findDeepestParenthesisIndex.cpp

--- tests/findDeepestParenthesisIndex.cpp
+++ tests/findDeepestParenthesisIndex.cpp
@@ -1,15 +1,15 @@
 #include <parenthesisMatch.h>
 #include <string>
 
-#define assert(var) if(!(var)) exit(-1)
+#define ASSERT(var) if(!(var)) exit(-1)
 
 int main() {
-  assert(findDeepestParenthesisIndex("()") == 0);
-  assert(findDeepestParenthesisIndex("(())") == 1);
-  assert(findDeepestParenthesisIndex("()()") == 0);
-  assert(findDeepestParenthesisIndex("()(())") == 3);
-  assert(findDeepestParenthesisIndex("(1-(2+3))") == 3);
-  assert(findDeepestParenthesisIndex("(1-((2*(14-2))-(14/2)))") == 7);
-  assert(findDeepestParenthesisIndex(" ()") == 1);
+  ASSERT(findDeepestParenthesisIndex("()") == 0);
+  ASSERT(findDeepestParenthesisIndex("(())") == 1);
+  ASSERT(findDeepestParenthesisIndex("()()") == 0);
+  ASSERT(findDeepestParenthesisIndex("()(())") == 3);
+  ASSERT(findDeepestParenthesisIndex("(1-(2+3))") == 3);
+  ASSERT(findDeepestParenthesisIndex("(1-((2*(14-2))-(14/2)))") == 7);
+  ASSERT(findDeepestParenthesisIndex(" ()") == 1);
   return 0;
 }
