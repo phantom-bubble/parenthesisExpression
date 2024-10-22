745219e791eb9f783166153a4de64dce2e54b310
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-22 22:05:45 +0800

    Update findDeepestParenthesis.cpp

--- tests/findDeepestParenthesis.cpp
+++ tests/findDeepestParenthesis.cpp
@@ -1,20 +1,21 @@
 #include <parenthesisMatch.h>
 #include <string>
-#include <assert.h>
+
+#define ASSERT(var) if(!(var)) exit(-1)
 
 int main() {
-  assert(findDeepestParenthesis("(1)") == "1");
-  assert(findDeepestParenthesis("((1))") == "1");
-  assert(findDeepestParenthesis("(1)(2)") == "1");
-  assert(findDeepestParenthesis("(1)((2))") == "2");
-  assert(findDeepestParenthesis("(1+(2-4)+5)") == "2-4");
-  assert(findDeepestParenthesis("(1+((1/2)*2-(2*4))+5)") == "1/2");
+  ASSERT(findDeepestParenthesis("(1)") == "1");
+  ASSERT(findDeepestParenthesis("((1))") == "1");
+  ASSERT(findDeepestParenthesis("(1)(2)") == "1");
+  ASSERT(findDeepestParenthesis("(1)((2))") == "2");
+  ASSERT(findDeepestParenthesis("(1+(2-4)+5)") == "2-4");
+  ASSERT(findDeepestParenthesis("(1+((1/2)*2-(2*4))+5)") == "1/2");
 
-  assert(findDeepestParenthesis("(1)", 0) == "(1)");
-  assert(findDeepestParenthesis("((1))", 0) == "(1)");
-  assert(findDeepestParenthesis("(1)(2)", 0) == "(1)");
-  assert(findDeepestParenthesis("(1)((2)), 0") == "(2)");
-  assert(findDeepestParenthesis("(1+(2-4)+5)", 0) == "(2-4)");
-  assert(findDeepestParenthesis("(1+((1/2)*2-(2*4))+5)", 0) == "(1/2)");
+  ASSERT(findDeepestParenthesis("(1)", 0) == "(1)");
+  ASSERT(findDeepestParenthesis("((1))", 0) == "(1)");
+  ASSERT(findDeepestParenthesis("(1)(2)", 0) == "(1)");
+  ASSERT(findDeepestParenthesis("(1)((2)), 0") == "(2)");
+  ASSERT(findDeepestParenthesis("(1+(2-4)+5)", 0) == "(2-4)");
+  ASSERT(findDeepestParenthesis("(1+((1/2)*2-(2*4))+5)", 0) == "(1/2)");
   return 0;
 }
