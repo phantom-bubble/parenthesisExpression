79a69475168fb73b9bba94fedd46f774e8d17e4b
泡泡 幻影 <435636890@qq.com>
2024-10-23 00:17:33 +0800

    测试案例更新

--- tests/findDeepestParenthesis.cpp
+++ tests/findDeepestParenthesis.cpp
@@ -10,7 +10,17 @@ int main() {
   ASSERT(findDeepestParenthesis("(1)((2))") == "2");
   ASSERT(findDeepestParenthesis("(1+(2-4)+5)") == "2-4");
   ASSERT(findDeepestParenthesis("(1+((1/2)*2-(2*4))+5)") == "1/2");
-  ASSERT(findDeepestParenthesis("(1+((1/2)*2-(2*4))+5)", 1) == "2*4");
+
+  //ASSERT(findDeepestParenthesis("(1+((1/2)*2-(2*4))+5)", 1) == "2*4");
+  for (int i = 0; i < 10; i++) {
+	  std::string a = "zzzzzzzZz";
+	  std::string b = a;
+	  for (int j = 0; j < i; j++) {
+		  b = "(" + b + ")");
+		  ASSERT(findDeepestParenthesis(b) ==a);
+	  }
+  }
+
   ASSERT(findDeepestParenthesis("(1)", 0) == "(1)");
   ASSERT(findDeepestParenthesis("((1))", 0) == "(1)");
   ASSERT(findDeepestParenthesis("(1)(2)", 0) == "(1)");
