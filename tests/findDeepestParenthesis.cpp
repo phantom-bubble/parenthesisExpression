58b50f1eebfcab437f0240e166b5bb7d166e665c
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-22 21:16:46 +0800

    Update findDeepestParenthesis.cpp

--- tests/findDeepestParenthesis.cpp
+++ tests/findDeepestParenthesis.cpp
@@ -7,10 +7,14 @@ int main() {
   assert(findDeepestParenthesis("((1))") == "1");
   assert(findDeepestParenthesis("(1)(2)") == "1");
   assert(findDeepestParenthesis("(1)((2))") == "2");
+  assert(findDeepestParenthesis("(1+(2-4)+5)") == "2-4");
+  assert(findDeepestParenthesis("(1+((1/2)*2-(2*4))+5)") == "1/2");
 
   assert(findDeepestParenthesis("(1)", 0) == "(1)");
   assert(findDeepestParenthesis("((1))", 0) == "(1)");
   assert(findDeepestParenthesis("(1)(2)", 0) == "(1)");
   assert(findDeepestParenthesis("(1)((2)), 0") == "(2)");
+  assert(findDeepestParenthesis("(1+(2-4)+5)", 0) == "(2-4)");
+  assert(findDeepestParenthesis("(1+((1/2)*2-(2*4))+5)", 0) == "(1/2)");
   return 0;
 }
