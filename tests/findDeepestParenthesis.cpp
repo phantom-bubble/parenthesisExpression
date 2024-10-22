3af8f3d45eac6aed7f8e1205348b72fa0870df68
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-22 22:16:47 +0800

    Update findDeepestParenthesis.cpp

--- tests/findDeepestParenthesis.cpp
+++ tests/findDeepestParenthesis.cpp
@@ -11,7 +11,7 @@ int main() {
   ASSERT(findDeepestParenthesis("(1+(2-4)+5)") == "2-4");
   ASSERT(findDeepestParenthesis("(1+((1/2)*2-(2*4))+5)") == "1/2");
 
-  // ASSERT(findDeepestParenthesis("(1)", 0) == "(1)");
+  ASSERT(findDeepestParenthesis("(1)", 0) == "(1)");
   // ASSERT(findDeepestParenthesis("((1))", 0) == "(1)");
   // ASSERT(findDeepestParenthesis("(1)(2)", 0) == "(1)");
   // ASSERT(findDeepestParenthesis("(1)((2)), 0") == "(2)");
