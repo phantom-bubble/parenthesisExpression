e1670b332e1910e47fddd8e7f019f5a44436b66f
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-22 21:11:43 +0800

    Update findDeepestParenthesis.cpp

--- tests/findDeepestParenthesis.cpp
+++ tests/findDeepestParenthesis.cpp
@@ -7,5 +7,10 @@ int main() {
   assert(findDeepestParenthesis("((1))") == "1");
   assert(findDeepestParenthesis("(1)(2)") == "1");
   assert(findDeepestParenthesis("(1)((2))") == "2");
+
+  assert(findDeepestParenthesis("(1)", 0) == "(1)");
+  assert(findDeepestParenthesis("((1))", 0) == "(1)");
+  assert(findDeepestParenthesis("(1)(2)", 0) == "(1)");
+  assert(findDeepestParenthesis("(1)((2)), 0") == "(2)");
   return 0;
 }
