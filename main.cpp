b7eb767f88409d310d2786accc01a030c41b3417
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-22 23:21:17 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main() {
 	std::string test = "(1$我锟斤拷-jab2(*烫烫烫dhidoaozzzZzw)-;2(&iwh）中文括号（72))()";
 	std::cout << findDeepestParenthesisIndex(test) << std::endl;
 	std::cout << findDeepestParenthesisIndex("((1+2)*3)") << std::endl;
-	// std::cout << evaluateString("1+2+3+(4-5)*6") << std::endl;
+	std::cout << evaluateString("1+2+3+(4+5)*6") << std::endl;
 //	std::cout << replaceDeepestParenthesisByString("1+2*(3+4)", additionCut(findDeepestParenthesis("(1+2*(3+4))"))) << std::endl;
 }
