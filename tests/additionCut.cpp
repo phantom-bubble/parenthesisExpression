4b79aa775bb3891fe343e28f92f1579122018194
phantom-bubble <166215777+phantom-bubble@users.noreply.github.com>
2024-10-22 17:10:58 +0800

    Update additionCut.cpp

--- tests/additionCut.cpp
+++ tests/additionCut.cpp
@@ -4,23 +4,23 @@
 #include<string>
 #include<sstream>
 
-#define ASSERT(var1, var2) if(var1 != var2) { std::cout << var1 << var2 << "\n"; exit(-1); }
+#define ASSERT(var1, var2) if(var1 != var2) { std::cerr << var1 << var2 << "\n"; exit(-1); }
 
 int main() {
   for(int i = 0; i < 2048; ++i) {
     for(int j = 0; j < 2048; ++j) {
-      std::string sum;
-      std::string var1;
-      std::string var2;
-      std::ostringstream os;
+      std::string sum = "";
+      std::string var1 = "";
+      std::string var2 = "";
+      std::ostringstream os{""};
       os.clear();
-      os << i << std::flush;
+      os << i;
       var1 = os.str();
       os.clear();
-      os << j << std::flush;
+      os << j;
       var2 = os.str();
       os.clear();
-      os << i + j << std::flush;
+      os << i + j;
       sum = os.str();
       ASSERT(sum, additionCut(var1 + "+" + var2))
     }
