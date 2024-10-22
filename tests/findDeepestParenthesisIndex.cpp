5b7dc2cd104107b5c2c2d2c2ed44579def6b79ef
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-22 21:45:07 +0800

    故意写错

--- tests/findDeepestParenthesisIndex.cpp
+++ tests/findDeepestParenthesisIndex.cpp
@@ -9,6 +9,6 @@ int main() {
   assert(findDeepestParenthesisIndex("()(())") == 3);
   assert(findDeepestParenthesisIndex("(1-(2+3))") == 3);
   assert(findDeepestParenthesisIndex("(1-((2*(14-2))-(14/2)))") == 7);
-  assert(findDeepestParenthesisIndex(" ()") == 1);
+  assert(findDeepestParenthesisIndex(" ()") == 0);
   return 0;
 }
