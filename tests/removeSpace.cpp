6b1f17a1ae6ff7eaa3ff8050766b3001fd6167d2
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-22 23:10:16 +0800

    Update removeSpace.cpp

--- tests/removeSpace.cpp
+++ tests/removeSpace.cpp
@@ -1,7 +1,7 @@
 #include <parenthesisMatch.h>
 #include <string>
 
-#define ASSERT(var) if(!(var)) exit(-1)
+#define ASSERT(var) if(!(var)) return -1
 
 int main() {
   ASSERT(removeSpace("1000 0000") == "10000000");
