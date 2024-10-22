7c27c1a21b654ff5788f253ba5f45a279e62d493
泡泡 幻影 <435636890@qq.com>
2024-10-22 23:35:54 +0800

    添加测试案例removespace

--- tests/removeSpace.cpp
+++ tests/removeSpace.cpp
@@ -6,5 +6,10 @@
 int main() {
   ASSERT(removeSpace("1000 0000") == "10000000");
   ASSERT(removeSpace("         ") == "");
+  ASSERT(removeSpace("a b c d") == "abcd");
+  ASSERT(removeSpace(" a  b c  d ") == "abcd");
+  //for (int i = 0; i < 25; i++) {
+	 // std::string a="vda"
+  //}
   return 0;
 }
