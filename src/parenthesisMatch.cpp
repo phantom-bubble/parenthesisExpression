45400326c3003be052a957bff37d8b53520e2b29
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-24 00:49:29 +0800

    Update parenthesisMatch.cpp

--- src/parenthesisMatch.cpp
+++ src/parenthesisMatch.cpp
@@ -58,27 +58,6 @@ std::string findDeepestParenthesis(std::string tmpstr, bool option=1) {
 	throw "error!";
 }
 
-std::string additionCut(std::string tmpstr) {
-	std::string outputstring1 = "";
-	std::string outputstring2 = "";
-	bool addFind = false;
-	for (int i = 0; i < tmpstr.size(); i++) {
-		if (tmpstr[i] != '+' && addFind == false) {
-			outputstring1 += tmpstr[i];
-		}
-		if (tmpstr[i] == '+') {
-			addFind = true;
-		}
-		if (tmpstr[i] != '+' && addFind == true) {
-			outputstring2 += tmpstr[i];
-		}
-	}
-	int outputint1 = std::stoi(outputstring1);
-	int outputint2 = std::stoi(outputstring2);
-	std::string outputstring = std::to_string(outputint1 + outputint2);
-	return outputstring;
-}
-
 //删除空格
 std::string removeSpace(std::string tmpstr) {
 	std::string outputstring = "";
