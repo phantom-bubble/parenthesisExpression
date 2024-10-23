c98e52028117f06c9fa27372afabf7fd64fffafc
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-24 00:54:01 +0800

    Update additionCut.cpp

--- tests/additionCut.cpp
+++ tests/additionCut.cpp
@@ -4,11 +4,11 @@
 #define ASSERT(var) if(!(var)) return -1
 
 int main() {
-  ASSERT("0" == additionCut("0+0"));
-  ASSERT("1" == additionCut("0+1"));
-  ASSERT("1" == additionCut("1+0"));
-  ASSERT("7777778" == additionCut("1+7777777"));
-  ASSERT("65536" == additionCut("1+65535"));
+  ASSERT("0" == addition("0 + 0"));
+  ASSERT("1" == addition("0 + 1"));
+  ASSERT("1" == addition("1 + 0"));
+  ASSERT("7777778" == addition("1 + 7777777"));
+  ASSERT("65536" == additionC("1 + 65535"));
   for(int i = 0; i < 2048; ++i) {
     for(int j = 0; j < 2048; ++j) {
       std::string sum = "";
@@ -17,7 +17,7 @@ int main() {
       var1 = std::to_string(i);
       var2 = std::to_string(j);
       sum = std::to_string(i + j);
-      ASSERT(sum == additionCut(var1 + "+" + var2));
+      ASSERT(sum == addition(var1 + " + " + var2));
     }
   }
   return 0;
