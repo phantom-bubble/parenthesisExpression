65a752d00afebe93dedc47fcb9e5ab6bee55b191
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-24 00:19:59 +0800

    Create subtraction.cpp

---
+++ src/subtraction.cpp
@@ -0,0 +1,9 @@
+std::string subtraction(std::string subtraction_expression) {
+	std::string outputstring1 = "";
+	std::string outputstring2 = "";
+  auto subtraction_operator_index = subtraction_expression.find('-');
+  std::string first_num = subtraction_expression.substr(0, subtraction_operator_index);
+  std::string second_num = substraction_expression.substr(subtraction_operator_index + 1);
+  auto result = std::to_string(std::stoi(first_num) + std::stoi(second_num));
+  return result;
+}
