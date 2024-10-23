e75ab72cfe4464b40ab840e187feda7c591d7138
泡泡 幻影 <435636890@qq.com>
2024-10-24 00:14:23 +0800

    substractionCut

--- include/parenthesisMatch.h
+++ include/parenthesisMatch.h
@@ -10,9 +10,22 @@
  */
 bool parenthesisMatch(std::string tmpstr);
 
+/**
+ * @brief 寻找最深层括号
+ * @param  tmpstr 待匹配的字符串
+ * @param  option 0表示保留括号 1表示删除括号
+ * @return std::string 最深层括号内的字符串
+ */
 std::string findDeepestParenthesis(std::string tmpstr, bool option=1);
 
+/**
+ * @brief 把字符串的加法表达式计算成结果
+ * @param  tmpstr 待计算的字符串
+ * @return string 计算结果（字符串）
+ */
 std::string additionCut(std::string tmpstr);
+
+std::string subtractionCut(std::string tmpstr);
 
 //删除空格
 std::string removeSpace(std::string tmpstr);
