126eaa121ca9e3fb92c593371a0e205b280c9da4
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-24 11:24:30 +0800

    Update addition.cpp

--- src/addition.cpp
+++ src/addition.cpp
@@ -1,8 +1,6 @@
 #include <string>
 
 std::string addition(std::string addition_expression) {
-	std::string outputstring1 = "";
-	std::string outputstring2 = "";
 	auto addition_operator_index = addition_expression.find(" + ");
 	std::string first_num = addition_expression.substr(0, addition_operator_index);
 	std::string second_num = addition_expression.substr(addition_operator_index + 3);
