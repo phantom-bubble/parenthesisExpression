5a5256964f598f9227a67ed95e9d8b68cd097482
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-24 11:54:26 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,5 +8,5 @@ int main() {
 	std::cout << findDeepestParenthesisIndex("((1+2)*3)") << std::endl;
 	std::cout << evaluateString("1+2+3+(4+5)*6") << std::endl;
 //	std::cout << replaceDeepestParenthesisByString("1+2*(3+4)", additionCut(findDeepestParenthesis("(1+2*(3+4))"))) << std::endl;
-	if(subtraction("5-1") != "4") exit(-1);
+	// if(subtraction("5-1") != "4") exit(-1);
 }
