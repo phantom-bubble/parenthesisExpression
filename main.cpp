c0ad138a3395149a9810eedfcb77c749b26e75c8
泡泡 幻影 <435636890@qq.com>
2024-10-28 00:02:20 +0800

    修改完毕

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include<parenthesisExpression.h>
 #include<iostream>
 
+#define ASSERT(var) if(!(var)) return -1
+
 int main() {
 	int a(1);
 	std::string test = "(1$我锟斤拷-jab2(*烫烫烫dhidoaozzzZzw)-;2(&iwh）中文括号（72))()";
 	std::cout << findDeepestParenthesisIndex(test) << std::endl;
 	std::cout << findDeepestParenthesisIndex("((1+2)*3)") << std::endl;
 	std::cout << evaluateString("1+2+3+(4+5)*6") << std::endl;
-	std::cout << subtraction("10 - 5") << std::endl;
-	std::cout << subtraction("10-5") << std::endl;
-//	std::cout << replaceDeepestParenthesisByString("1+2*(3+4)", additionCut(findDeepestParenthesis("(1+2*(3+4))"))) << std::endl;
-	// if(subtraction("5-1") != "4") exit(-1);
+    ASSERT("0" == subtraction("0 - 0"));
+    ASSERT("-1" == subtraction("0 - 1"));
+    ASSERT("1" == subtraction("1 - 0"));
+    ASSERT("7777776" == subtraction("7777777 - 1"));
+    ASSERT("-1" == subtraction("1 - 2"));
+    ASSERT("-65536" == subtraction("-1 - 65535"));
+    for (int i = 2048; i > 0; --i) {
+        for (int j = 0; j < 2048; ++j) {
+            std::string sum = "";
+            std::string var1 = "";
+            std::string var2 = "";
+            var1 = std::to_string(i);
+            var2 = std::to_string(j);
+            sum = std::to_string(i - j);
+            ASSERT(sum == subtraction(var1 + " - " + var2));
+        }
+    }
+    return 0;
 }
