4056ccd37a5afe47e1b1316f55ef444d679efaf6
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-28 15:11:07 +0800

    补充了乘法测试案例

--- tests/multiplication.cpp
+++ tests/multiplication.cpp
@@ -1,5 +1,6 @@
 #include <parenthesisExpression.h>
 #include <string>
+#include <stdlib.h>
 
 #define ASSERT(var) if(!(var)) return -1
 
@@ -8,5 +9,11 @@ int main() {
 	ASSERT(multiplication("1 * 0") == "0");
 	ASSERT(multiplication("0 * 2") == "0");
 	ASSERT(multiplication("1 * 2") == multiplication("-2 * -1"));
+	for(int i = 0; i < 10000000; ++i) {
+		int var1 = rand() % 100000;
+		int var2 = rand() % 10000;
+		int result = var1 * var2;
+		ASSERT(std::to_string(result) == multiplication(std::to_string(var1) + " * " + std::to_string(var2));
+	}
 	return 0;
 }
