1bfc51e58448e7eaa70fe9bf9c3f79d7a5b41795
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-28 11:38:56 +0800

    Update multiplication.cpp

--- tests/multiplication.cpp
+++ tests/multiplication.cpp
@@ -4,5 +4,9 @@
 #define ASSERT(var) if(!(var)) return -1
 
 int main() {
+	ASSERT(multiplication("1 * 2") == "2");
+	ASSERT(multiplication("1 * 0") == "0");
+	ASSERT(multiplication("0 * 2") == "0");
+	ASSERT(multiplication("1 * 2") == multiplication("-2 * -1"));
 	return 0;
 }
