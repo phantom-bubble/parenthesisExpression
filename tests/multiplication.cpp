13850a612ebb243b3d271b6b9a0d909c09470a62
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-28 15:12:34 +0800

    修复了缺少反括号的bug

--- tests/multiplication.cpp
+++ tests/multiplication.cpp
@@ -13,7 +13,7 @@ int main() {
 		int var1 = rand() % 100000;
 		int var2 = rand() % 10000;
 		int result = var1 * var2;
-		ASSERT(std::to_string(result) == multiplication(std::to_string(var1) + " * " + std::to_string(var2));
+		ASSERT(std::to_string(result) == multiplication(std::to_string(var1) + " * " + std::to_string(var2)));
 	}
 	return 0;
 }
