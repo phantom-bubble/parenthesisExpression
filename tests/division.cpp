b7822f5d0d414b44bc0f90ace084ad6ee0f16ba9
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-28 13:27:40 +0800

    Update division.cpp

--- tests/division.cpp
+++ tests/division.cpp
@@ -1,9 +1,16 @@
 #include <parenthesisExpression.h>
 #include <string>
+#include <rand.h>
 
 #define ASSERT(var) if(!(var)) return -1
 
 int main() {
 	ASSERT("2" == division("4 / 2"));
+	for(int i = 0; i < 1000000; ++i) {
+		int var1 = rand();
+		int var2 = rand();
+		int result = var1 / var2;
+		ASSERT(result == division(std::to_string(var1) + " / " + std::to_string(var2)));
+	}
 	return 0;
 }
