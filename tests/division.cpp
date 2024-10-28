0f5976321e73a280b86971825966c716370184db
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-28 14:01:46 +0800

    修复除零bug

--- tests/division.cpp
+++ tests/division.cpp
@@ -9,6 +9,8 @@ int main() {
 	for(int i = 0; i < 10000000; ++i) {
 		int var1 = rand();
 		int var2 = rand();
+		if(var2 == 0)
+			var2 = 1;
 		int result = var1 / var2;
 		ASSERT(std::to_string(result) == division(std::to_string(var1) + " / " + std::to_string(var2)));
 	}
