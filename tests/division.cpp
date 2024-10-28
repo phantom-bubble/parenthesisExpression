e78dfc3ec8321bbc514feb7f43d20d593c915eab
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-28 13:30:21 +0800

    修复数字与字符串比较的bug

--- tests/division.cpp
+++ tests/division.cpp
@@ -10,7 +10,7 @@ int main() {
 		int var1 = rand();
 		int var2 = rand();
 		int result = var1 / var2;
-		ASSERT(result == division(std::to_string(var1) + " / " + std::to_string(var2)));
+		ASSERT(std::to_string(result) == division(std::to_string(var1) + " / " + std::to_string(var2)));
 	}
 	return 0;
 }
