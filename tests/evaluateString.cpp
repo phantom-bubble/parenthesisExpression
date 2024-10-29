07cc5afa8ba6d47581bd779cb18a386c1fb61330
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-29 15:53:47 +0800

    Update evaluateString.cpp

--- tests/evaluateString.cpp
+++ tests/evaluateString.cpp
@@ -1,13 +1,9 @@
 #include <parenthesisExpression.h>
 #include <string>
-#include <iostream>
 
 #define ASSERT(var) if(!(var)) return -1
 
 int main() {
-  std::cout << evaluateString("1 - (2 + 3)") << "\n";
-  std::cout << evaluateString("(5 - 3)") << "\n";
-  std::cout << evaluateString("(4 + 5) - (2 + 3)") << "\n";
   ASSERT(evaluateString("1 - (2 + 3)") == "-4");
   ASSERT(evaluateString("(5 - 3)") == "2");
   ASSERT(evaluateString("(4 + 5) - (2 + 3)") == "4");
