4ed8b9976bc63350a31bf091de030d2011cb3f97
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-29 13:42:43 +0800

    递归

--- src/evaluateString.cpp
+++ src/evaluateString.cpp
@@ -2,10 +2,19 @@
 #include <string>
 
 std::string evaluateString(std::string tmpstr) {
+  if(!containParenthesisExpression(tmpstr)) {
+    std::string sub_expression = findDeepestParenthesis(tmpstr);
+    if(isAdditionExpression(sub_expression))
+      sub_expression = addition(sub_expression);
+    else if(isSubtractionExpression(sub_expression))
+      sub_expression = subtraction(sub_expression);
+    return replaceDeepestParenthesisByString(tmpstr, sub_expression);
+  }
   std::string sub_expression = findDeepestParenthesis(tmpstr);
   if(isAdditionExpression(sub_expression))
     sub_expression = addition(sub_expression);
   else if(isSubtractionExpression(sub_expression))
     sub_expression = subtraction(sub_expression);
-  return replaceDeepestParenthesisByString(tmpstr, sub_expression);
+  tmpstr = replaceDeepestParenthesisByString(tmpstr, sub_expression);
+  return evaluateString(tmpstr);
 }
