eafb2708f234c1788fa41a70fd090efdab475fdd
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-29 11:10:37 +0800

    添加减法判断

--- src/evaluateString.cpp
+++ src/evaluateString.cpp
@@ -5,5 +5,7 @@ std::string evaluateString(std::string tmpstr) {
   std::string sub_expression = findDeepestParenthesis(tmpstr);
   if(isAdditionExpression(sub_expression))
     sub_expression = addition(sub_expression);
-	return replaceDeepestParenthesisByString(tmpstr, sub_expression);
+  else if(isSubtractionExpression(sub_expression))
+    sub_expression = subtraction(sub_expression);
+  return replaceDeepestParenthesisByString(tmpstr, sub_expression);
 }
