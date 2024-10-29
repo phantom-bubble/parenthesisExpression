b2d52538da1878d9cf3eb8b03ed3878c0aebe52c
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-29 16:50:52 +0800

    Update evaluateString.cpp

--- tests/evaluateString.cpp
+++ tests/evaluateString.cpp
@@ -7,5 +7,6 @@ int main() {
   ASSERT(evaluateString("1 - (2 + 3)") == "-4");
   ASSERT(evaluateString("(5 - 3)") == "2");
   ASSERT(evaluateString("(4 + 5) - (2 + 3)") == "4");
+  ASSERT(evaluateString("((15 + 200) - (0 - 50))") == "265");
   return 0;
 }
