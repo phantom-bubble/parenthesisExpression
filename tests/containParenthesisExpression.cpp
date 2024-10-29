a3a7c0fd10c64705193bc1c529acf651f34df239
zzzzzzzZz-chow <134291306+zzzzzzzZz-chow@users.noreply.github.com>
2024-10-29 13:28:49 +0800

    Create containParenthesisExpression.cpp

---
+++ tests/containParenthesisExpression.cpp
@@ -0,0 +1,11 @@
+#include <parenthesisExpression.cpp>
+#include <string>
+
+#define ASSERT(var) if(!(var)) return -1
+
+int main() {
+  ASSERT(containParenthesisExpression("1 + 1") == false);
+  ASSERT(containParenthesisExpression("(1 + 1)") == true);
+  ASSERT(containParenthesisExpression("1 + (1 + 1)") == true);
+  return 0;
+}
